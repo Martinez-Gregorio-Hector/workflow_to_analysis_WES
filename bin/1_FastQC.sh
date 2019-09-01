3936b1bdd040a39faae14be14b9ea3da1e64dda3
Martínez-Gregorio Héctor <53798505+Martinez-Gregorio-Hector@users.noreply.github.com>
2019-08-31 20:11:07 -0500

    Update

---
+++ bin/1_FastQC.sh
@@ -0,0 +1,16 @@
+## Once the software was downloaded and added on the PATH
+## Run this command to evalute the quality of raw data
+
+## make a directory when put the fastqc reports
+
+mkdir -p fastqc_reports
+
+## Perform the QC analysis on the raw data using FASTQC
+
+for i in *.fastq; do
+fastqc $i 
+done 
+
+## move your results in fastqc_reports
+
+mv *.html *.zip fastcq_reports
