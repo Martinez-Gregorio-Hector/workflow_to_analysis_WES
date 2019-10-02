4fe5b44233dc33a69848d27dbbec32f370466f4e
Martínez-Gregorio Héctor <53798505+Martinez-Gregorio-Hector@users.noreply.github.com>
2019-10-02 14:45:06 -0500

    Update

--- bin/2_Alignment.sh
+++ bin/2_Alignment.sh
@@ -1,11 +1,28 @@
-## Alignment 
+######### Alignment ###########
 
-mkdir -p sam
+######### Code to aligned fastqc to sam ###
 
-bwa mem -M -t15 - R \
-"@RG\tID:\tSM:\tPL:\tLB:\tPU:" \
-../../ref/ucsc.hg19.fasta \
-READ1.fastq READ2.fastq > READ_aligned.sam
+bwa mem -M -t 6 -R "@RG\tID:\tSM:\tPL:\tPU:\tLB:" [ref.fasta] readN.fastq > aligned.sam
 
-mv *.sam 
+
+-M  if a read is split (different parts map to different places) mark all parts other than main as "secondary alignement"
+-R add Read Group description (tID:id.sample\tSM:sample\tPL:plataform\tPU:platform.used\tLB:type.of.library)
+-t treads
+-readN.fastq Can be single-end or paired-end 
+
+### Doing loops for samples that will be analyzed
+
+# the files look like as Tumor1.fastq.tar.gz 
+
+tar -zxvf .tar.gz # extract the file to generate files Tumor1.fastq ... TumorN.fastq
+
+mkdir -p ../sam
+
+
+for prefix in $(ls *.fastq | rev | cut -c 7- | rev | uniq); do
+bwa mem -M -t 10 -R "@RG\tID:${prefix}\tSM:${prefix}\tPL:illumina\tPU:Lane1\tLB:exome" \
+[reference.fasta] ${prefix}.fastq > ../sam/${prefix}.aligned.sam
+done
+
+
 
