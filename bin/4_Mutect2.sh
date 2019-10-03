76841eb7b20c39a121f2b9b741142ffc5b75655f
Martínez-Gregorio Héctor <53798505+Martinez-Gregorio-Hector@users.noreply.github.com>
2019-10-03 16:28:33 -0500

    Update

--- bin/4_Mutect2.sh
+++ bin/4_Mutect2.sh
@@ -1,1 +1,25 @@
 
+## Once the software was downloaded and added on the PATH
+## This script is used for calling SNV using Mutect2 (inside in GATK)
+
+## ##### Your data could be organized in this form
+
+# bin
+# data
+##  01_fastq ##
+##  02_Sam ## R
+
+# This sceript is using only tumor sample
+
+
+mkdir -p ../data/Gatk07_Mutect2
+
+for i in $(ls *.bam | rev | cut -c 38- | rev | uniq); do
+gatk Mutect2 -R /data/Lab13/ref/ucsc.hg19.fasta -I ${i%.*}.aligned.sorted.Dedup.recal.reads.bam -tumor $i  --disable-read-filter MateOnSameContigOrNoMappedMateReadFilter -O ${i%.*}.vcf.gz
+done  
+
+mv *.vcf ../Mutect2/
+
+#######
+
+echo done
