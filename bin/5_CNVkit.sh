8b8ae3e798597afb529bd76b3f9cdcb9c0224114
Martínez-Gregorio Héctor <53798505+Martinez-Gregorio-Hector@users.noreply.github.com>
2019-10-20 19:47:33 -0500

    Update

--- bin/5_CNVkit.sh
+++ bin/5_CNVkit.sh
@@ -1,1 +1,26 @@
+## Once the software was downloaded and added on the PATH follow this script 
+##### Your data could be organized in this form
+
+```
+  bin
+  data
+      Tumoral ### Contains bams of tumoral samples
+      Normal  ### Contains bams of normal samples
+```
+
+## This script is to generate cns and cnr which is used to call somatic copy number variations (sCNV)
+
+mkdir ../data/results_cnvkit
+
+cnvkit.py batch ../data/Tumoral/*.bam --normal ../data/Normal/*.bam \
+Exome_Probes.bed --annotate refFlat(Is optional) --fasta "reference.fasta" \
+--access /cnvkit/data/access-5k-mappable.hg19.bed --output-reference my_reference \
+--output-dir ../data/results_cnvkit --diagram --scatter
+
+## This script is to call sCNV
+
+mkdir ../data/call_cnv
+
+cnvkit.py call *.cns -o mkdir ../data/call_cnv
+
 
