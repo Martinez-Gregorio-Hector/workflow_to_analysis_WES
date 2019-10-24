c35b11c9a5469bff74bfabb2197af5b58eda7f0d
Martínez-Gregorio Héctor <53798505+Martinez-Gregorio-Hector@users.noreply.github.com>
2019-10-24 10:18:15 -0500

    Update

--- bin/merged.sh
+++ bin/merged.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## This command is an issue that i need to correct
+
 awk '{OFS="\t";print "TNBC_033", $0}' TNBC_033.txt  > merged.txt  \
 awk '{OFS="\t";print "TNBC_034", $0}' TNBC_034.txt | sed 1d -  >> merged.txt \
 awk '{OFS="\t";print "TNBC_035", $0}' TNBC_035.txt | sed 1d -  >> merged.txt 
