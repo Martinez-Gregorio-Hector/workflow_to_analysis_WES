0f4f50f3ba95d779f553a9d7540a9539b7f4f68b
Martínez-Gregorio Héctor <53798505+Martinez-Gregorio-Hector@users.noreply.github.com>
2019-11-13 15:42:49 -0600

    Update 2_Alignment.sh

--- bin/2_Alignment.sh
+++ bin/2_Alignment.sh
@@ -1,22 +1,21 @@
 ######### Alignment ###########
-
 ######### Code to aligned fastqc to sam ###
 
-bwa mem -M -t 6 -R "@RG\tID:\tSM:\tPL:\tPU:\tLB:" [ref.fasta] readN.fastq > aligned.sam
+# bwa mem -M -t 6 -R "@RG\tID:\tSM:\tPL:\tPU:\tLB:" [ref.fasta] readN.fastq > aligned.sam
 
+# When
+# -M  if a read is split (different parts map to different places) mark all parts other than main as "secondary alignement"
+# -R add Read Group description (tID:id.sample\tSM:sample\tPL:plataform\tPU:platform.used\tLB:type.of.library)
+# -t treads 
+# -readN.fastq Can be single-end or paired-end 
 
--M  if a read is split (different parts map to different places) mark all parts other than main as "secondary alignement"
--R add Read Group description (tID:id.sample\tSM:sample\tPL:plataform\tPU:platform.used\tLB:type.of.library)
--t treads 
--readN.fastq Can be single-end or paired-end 
-
-### Doing loops for samples that will be analyzed
+### Doing loops for samples that will be analyzed.
 
 # the files look like as Tumor1.fastq.tar.gz 
 
-tar -zxvf .tar.gz # extract the file to generate files Tumor1.fastq ... TumorN.fastq
+tar -zxvf *.tar.gz # Extract the file in TumorN.fastq.tar.gz to generate files TumorN.fastq
 
-mkdir -p ../01_Sam
+mkdir -p ../01_Sam # Make a directory when is saved the alignments
 
 
 for prefix in $(ls *.fastq | rev | cut -c 7- | rev | uniq); do
